076c5e251cdf583f45553b2da951a4793948ef8b
yeeun0118s <yeeun0118s@naver.com>
2024-04-09 11:18:31 +0900

    Scaffold

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: StorePage(),
+    );
+  }
+}
+
+class StorePage extends StatelessWidget {
+  const StorePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Text("Woman"),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
